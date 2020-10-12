c5c30ca668b1d5426fe9de3ccbb87b8488daf7ef
Peyton Schlafley <pschlafley0@gmail.com>
2020-10-12 17:53:26 -0400

    deleted develop folder

---
+++ server.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const app = express();
+// const fs = require('fs');
+// const path = require('path');
+// const { notes } = require('./Develop/db/db.json');
+
+
+// // app.get('/api/notes', (req, res) => {
+// //     res.send('Hello!');
+// // });
+
+// app.listen(3001, () => {
+//     console.log(`API server now on port 3001!`);
+// });
